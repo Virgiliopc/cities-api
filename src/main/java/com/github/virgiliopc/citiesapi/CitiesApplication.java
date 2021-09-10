95dedd063ff1e81d971c4aea31e04b0a55a34558
Virgiiopc <virgilio.pires.costa@gmail.com>
2021-09-09 21:43:01 -0300

    New Project Adjustments citiesApplication.java and CountryResource.java api-cities files

--- src/main/java/com/github/virgiliopc/citiesapi/CitiesApplication.java
+++ src/main/java/com/github/virgiliopc/citiesapi/CitiesApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CitiesApplication {
 
-	public static void main(final String[] args) {
+	public static void main(String[] args) {
+
 		SpringApplication.run(CitiesApplication.class, args);
 	}
 }
--- src/main/java/com/github/virgiliopc/citiesapi/CountryResource.java
+++ src/main/java/com/github/virgiliopc/citiesapi/CountryResource.java
@@ -4,9 +4,13 @@ import com.github.virgiliopc.citiesapi.countries.Country;
 import com.github.virgiliopc.citiesapi.repository.CountryRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -15,11 +19,24 @@ public class CountryResource {
     private CountryRepository repository;
 
     public CountryResource(CountryRepository repository) {
+
         this.repository = repository;
     }
 
     @GetMapping
     public Page<Country> countries(Pageable page) {
+
         return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Country> getOne(@PathVariable Long id) {
+        Optional<Country> optional = repository.findById(id);
+
+        if (optional.isPresent()) {
+            return ResponseEntity.ok().body(optional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
